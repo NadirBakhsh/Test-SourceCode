d40b2ff1ce5f070e826e83582086915b3a12efc9
NadirBakhsh <nadir4k2010@gmail.com>
2024-01-20 04:06:01 +0500

    project init

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
+export default function Home() {
+  return (
+    <main className="relative">
+
+    </main>
+  );
+}
